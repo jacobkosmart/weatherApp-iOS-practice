7bcf59e138a0d08282566c0a159db9d2581f878a
jacobkosmart <jacobkosmart@gmail.com>
2021-12-17 21:10:50 +0900

    Initial Commit

---
+++ 06_weather_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  06_weather_app
+//
+//  Created by Jacob Ko on 2021/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
